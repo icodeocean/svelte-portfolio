104495e96f1938d514232ad3be70bf9904fdcfd8
icodeocean <justinytran@gmail.com>
2021-08-17 18:15:59 +0200

    per favore?

---
+++ src/images/__layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+</script>
+
+<Nav></Nav>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,800;1,100;1,200;1,500;1,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
+<style>
+    body{
+        background-color: #FFF3C7;
+        font-family: Poppins;
+    }
+	h1{
+		font-family: 'Libre Baskerville', serif;
+	}
+</style>
+
+</svelte:head>
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
+<slot></slot>
+
+
---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+<h1>salve</h1>
+
+
