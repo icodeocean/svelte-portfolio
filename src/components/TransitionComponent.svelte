59a2862fef35943e8151e1808f721bc93b6dab2b
icodeocean <justinytran@gmail.com>
2021-11-24 12:42:33 +0100

    Removed transitions

--- src/components/TransitionComponent.svelte
+++ src/components/TransitionComponent.svelte
@@ -1,10 +1,6 @@
 <script>
-	import { fly } from 'svelte/transition';
 </script>
 
-<div
-	in:fly|local={{ x: -100, duration: 200, delay: 350 }}
-	out:fly|local={{ x: 100, duration: 300 }}
->
+<div>
 	<slot />
 </div>
